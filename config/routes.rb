adaf80d42fa54a548e7d250c6700402108ffa49b
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-09-12 20:22:10 +0300

    feat: user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root "static_pages#home"
+  get "help" => "static_pages#help"
+  get "about" => "static_pages#about"
+  get "contact" => "static_pages#contact"
+  get "signup" => "users#new"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,5 +19,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root "static_pages#home"
 end
