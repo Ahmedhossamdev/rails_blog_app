441f39bf9a01ccf2208fb36e0df212710775aa4f
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-09-22 21:15:44 +0300

    Update action mailer configuration in production environment: Change authentication method to symbol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   port:                 ENV['SMTP_PORT'],
   user_name:            ENV['SMTP_USERNAME'],
   password:             ENV['SMTP_PASSWORD'],
-  authentication:       'plain',
+  authentication:       :plain,
   enable_starttls_auto: true
   }
 
