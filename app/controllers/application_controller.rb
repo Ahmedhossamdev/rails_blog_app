db7be732ca5cdf0edd70fd7e241ebab919dcfe09
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-09-25 02:33:40 +0300

    Refactor ApplicationController to include a logged_in_user method for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def hello
-    render html: "hello, world!"
+
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
   end
 
   include SessionsHelper
