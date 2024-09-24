32248ca71e86a76d8b7add23f0bd40c1d9509dfc
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-09-25 02:33:12 +0300

    Refactor User model and add feed method

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,11 +45,15 @@ class User < ApplicationRecord
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
   end
+
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
 
-
+  # Defines a proto-feed.
+  def feed
+    microposts
+  end
 
   # Sets the password reset attributes.
   def create_reset_digest
