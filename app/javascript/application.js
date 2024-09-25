cc85a787c25de2b529a749e57cee3476a186b3ae
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-09-25 04:59:33 +0300

    Refactor application.js to update Turbo import and disable Turbo session drive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
 import "controllers"
 import "custom/menu"
 import "custom/image_upload"
 //= require rails-ujs
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
